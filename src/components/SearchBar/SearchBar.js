16b6e7dbc2f9fdf4b26656f460cd8d5a7c6f02bf
Peng You <ypenggg11@gmail.com>
2023-03-21 15:56:09 +0000

    WIP: SearchBar logic

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, InputAdornment } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
+  const [search, setSearch] = useState("");
+
+  const changeSearchHandler = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
-    <React.Fragment>
-      <TextField
-        id='search-bar-with-icon'
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position='start'>
-              <SearchIcon />
-            </InputAdornment>
-          ),
-        }}
-        variant='outlined'
-        className="search-bar"
-      />
-    </React.Fragment>
+    <TextField
+      id='search-bar-with-icon'
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position='start'>
+            <SearchIcon className='search-bar__icon' />
+          </InputAdornment>
+        ),
+      }}
+      variant='outlined'
+      className='search-bar'
+      value={search}
+      onChange={changeSearchHandler}
+    />
   );
 };
 
