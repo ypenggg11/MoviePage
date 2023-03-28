5ff7e29b0f51c00873773d59eae9ee36ced56c48
Peng You <ypenggg11@gmail.com>
2023-03-28 10:52:29 +0100

    Minor fix using useSearchParams from React Router

--- src/components/Pagination/PaginationComponent.js
+++ src/components/Pagination/PaginationComponent.js
@@ -1,29 +1,30 @@
 import React, { useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 import PaginationContext from "../../store/pagination-context";
 
-import {NavigationButtonComponent} from "../../components";
+import { NavigationButtonComponent } from "../../components";
 
 /* Displays the current page index, and two button for navigate back or forward */
 export const PaginationComponent = () => {
   const { page, maxPages, slideChangeHandler } = useContext(PaginationContext);
-  const { pathname } = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <div className='page-nav'>
       {/* Page Index */}
-      <p className='page-nav__index'>{page}</p>
+      <p className='page-nav__index'>{searchParams.get("page")}</p>
       <span className='page-nav__buttons'>
         {/* Prev Page button */}
         {page > 1 && (
           <NavigationButtonComponent
             onClick={() => {
               slideChangeHandler("right");
+              setSearchParams({ page: +page - 1 });
             }}
             aria-label='left-nav'
           >
-            <Link to={`${pathname}?page=${+page - 1}`}>&lt;</Link>
+            &lt;
           </NavigationButtonComponent>
         )}
         {/* Next Page button */}
@@ -31,10 +32,11 @@ export const PaginationComponent = () => {
           <NavigationButtonComponent
             onClick={() => {
               slideChangeHandler("left");
+              setSearchParams({ page: +page + 1 });
             }}
             aria-label='right-nav'
           >
-            <Link to={`${pathname}?page=${+page + 1}`}>&gt;</Link>
+            &gt;
           </NavigationButtonComponent>
         )}
       </span>
